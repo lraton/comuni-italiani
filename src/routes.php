08eb9742750c287bc2beba3ef1a5578dcb3d1abf
Filippo Notari <50119007+lraton@users.noreply.github.com>
2026-07-29 12:44:54 +0200

    Convert Eloquent model responses to arrays
    Avoid caching raw Eloquent collections (Zip, City, Province) that were
    restored as __PHP_Incomplete_Class and caused TypeError 500 errors.

--- src/routes.php
+++ src/routes.php
@@ -13,25 +13,25 @@ use Illuminate\Support\Str;
 Route::group(['middleware' => ['api'], 'prefix' => config('comuni.route')], function () {
     Route::middleware(config('comuni.middlewares'))->get('/zones', function () {
         return Cache::remember('zones', config('comuni.ttl'), function () {
-            return Zone::orderBy('name', 'asc')->get();
+            return Zone::orderBy('name', 'asc')->get()->toArray();
         });
     });
 
     Route::middleware(config('comuni.middlewares'))->get('/zones/{id}', function ($id) {
         return Cache::remember('zones-'.$id, config('comuni.ttl'), function () use ($id) {
-            return Zone::where('id', $id)->with(['regions', 'regions.provinces', 'regions.provinces.cities', 'regions.provinces.cities.zips'])->firstOrFail();
+            return Zone::where('id', $id)->with(['regions', 'regions.provinces', 'regions.provinces.cities', 'regions.provinces.cities.zips'])->firstOrFail()->toArray();
         });
     })->whereNumber('id');
 
     Route::middleware(config('comuni.middlewares'))->get('/regions', function () {
         return Cache::remember('regions', config('comuni.ttl'), function () {
-            return Region::orderBy('name', 'asc')->get();
+            return Region::orderBy('name', 'asc')->get()->toArray();
         });
     });
 
     Route::middleware(config('comuni.middlewares'))->get('/regions/{id}', function ($id) {
         return Cache::remember('regions-'.$id, config('comuni.ttl'), function () use ($id) {
-            return Region::where('id', $id)->with(['zone', 'provinces', 'provinces.cities', 'provinces.cities.zips'])->firstOrFail();
+            return Region::where('id', $id)->with(['zone', 'provinces', 'provinces.cities', 'provinces.cities.zips'])->firstOrFail()->toArray();
         });
     })->whereNumber('id');
 
@@ -41,19 +41,19 @@ Route::group(['middleware' => ['api'], 'prefix' => config('comuni.route')], func
         }
 
         return Cache::remember('provinces', config('comuni.ttl'), function () {
-            return Province::orderBy('name', 'asc')->get();
+            return Province::orderBy('name', 'asc')->get()->toArray();
         });
     });
 
     Route::middleware(config('comuni.middlewares'))->get('/provinces/{code}', function ($code) {
         return Cache::remember('provinces-'.$code, config('comuni.ttl'), function () use ($code) {
-            return Province::where('code', $code)->with(['region', 'cities', 'cities.zips', 'region.zone'])->firstOrFail();
+            return Province::where('code', $code)->with(['region', 'cities', 'cities.zips', 'region.zone'])->firstOrFail()->toArray();
         });
     })->whereAlpha('code');
 
     Route::middleware(config('comuni.middlewares'))->get('/provinces/{id}', function ($id) {
         return Cache::remember('provinces-'.$id, config('comuni.ttl'), function () use ($id) {
-            return Province::where('id', $id)->with(['region', 'cities', 'cities.zips', 'region.zone'])->firstOrFail();
+            return Province::where('id', $id)->with(['region', 'cities', 'cities.zips', 'region.zone'])->firstOrFail()->toArray();
         });
     })->whereNumber('id');
 
@@ -63,7 +63,7 @@ Route::group(['middleware' => ['api'], 'prefix' => config('comuni.route')], func
         }
 
         return Cache::remember('cities', config('comuni.ttl'), function () {
-            return City::orderBy('name', 'asc')->get();
+            return City::orderBy('name', 'asc')->get()->toArray();
         });
     });
 
@@ -79,13 +79,13 @@ Route::group(['middleware' => ['api'], 'prefix' => config('comuni.route')], func
         }
 
         return Cache::remember('zips', config('comuni.ttl'), function () {
-            return Zip::orderBy('code', 'asc')->get();
+            return Zip::orderBy('code', 'asc')->get()->toArray();
         });
     });
 
     Route::middleware(config('comuni.middlewares'))->get('/zips/{id}', function ($id) {
         return Cache::remember('zips-'.$id, config('comuni.ttl'), function () use ($id) {
-            return Zip::where('id', $id)->with(['city', 'city.province', 'city.province.region', 'city.province.region.zone'])->firstOrFail();
+            return Zip::where('id', $id)->with(['city', 'city.province', 'city.province.region', 'city.province.region.zone'])->firstOrFail()->toArray();
         });
     })->whereNumber('id');
 });
